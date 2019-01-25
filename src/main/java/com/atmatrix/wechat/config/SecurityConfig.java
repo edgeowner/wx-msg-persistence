cecaaf848155b837b61fe1904e4df87612330471
edgeowner <2444039093@qq.com>
2019-01-25 15:44:26 +0800

    change_security_config

--- src/main/java/com/atmatrix/wechat/config/SecurityConfig.java
+++ src/main/java/com/atmatrix/wechat/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.atmatrix.wechat.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,12 +21,36 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic().and()
+        http.csrf().disable()
                 .authorizeRequests()
+                .antMatchers(
+                        HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "/favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js",
+                        "/webjars/**",
+                        "/swagger-resources/**",
+                        "/*/api-docs"
+                ).permitAll()
                 .antMatchers("/actuator/**").authenticated()
                 .anyRequest().permitAll();
 
 
     }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.GET, "*.js")
+                .antMatchers("/msg/**")
+                .antMatchers("/druid/**")
+                .antMatchers(HttpMethod.GET, ".html");
+
+    }
+
 
 }
--- src/main/java/com/atmatrix/wechat/middleware/kafka/consumer/KafkaConsumer.java
+++ src/main/java/com/atmatrix/wechat/middleware/kafka/consumer/KafkaConsumer.java
@@ -1,28 +1,28 @@
-//package com.atmatrix.wechat.middleware.kafka.consumer;//package com.atmatrix.wechat.middleware.kafka.consumer;
-//
-//import com.atmatrix.wechat.application.WechatMessageApplication;
-//import com.atmatrix.wechat.middleware.kafka.beans.WechatTextDto;
-//import com.google.gson.Gson;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.kafka.annotation.KafkaListener;
-//import org.springframework.stereotype.Component;
-//
-//import java.text.MessageFormat;
-//
-//@Slf4j
-//@Component("kafkaConsumer")
-//public class KafkaConsumer {
-//
-//    @Autowired
-//    private WechatMessageApplication wchatMessageApplication;
-//
-//    @KafkaListener(id = "wx_msg_id_2", topics = {"draftelf.draft.wechat.raw"})
-//    public void listen(String data) {
-//        Gson gson = new Gson();
-//        WechatTextDto wechatTextDto = gson.fromJson(data, WechatTextDto.class);
-//        wchatMessageApplication.saveWechatMessageMQ(wechatTextDto);
-//        log.info(MessageFormat.format("KafkaConsumer收到消息：{0}", data));
-//    }
-//
-//}
+package com.atmatrix.wechat.middleware.kafka.consumer;//package com.atmatrix.wechat.middleware.kafka.consumer;
+
+import com.atmatrix.wechat.application.WechatMessageApplication;
+import com.atmatrix.wechat.middleware.kafka.beans.WechatTextDto;
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.text.MessageFormat;
+
+@Slf4j
+@Component("kafkaConsumer")
+public class KafkaConsumer {
+
+    @Autowired
+    private WechatMessageApplication wchatMessageApplication;
+
+    @KafkaListener(id = "wx_msg_id_2", topics = {"draftelf.draft.wechat.raw"})
+    public void listen(String data) {
+        Gson gson = new Gson();
+        WechatTextDto wechatTextDto = gson.fromJson(data, WechatTextDto.class);
+        wchatMessageApplication.saveWechatMessageMQ(wechatTextDto);
+        log.info(MessageFormat.format("KafkaConsumer收到消息：{0}", data));
+    }
+
+}
